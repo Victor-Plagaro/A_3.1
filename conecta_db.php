deaab49cca55ce7d517bf25de05f9bf039460fa3
Victor Plagaro Moreno <plagaro26@gmail.com>
2023-10-27 12:04:00 +0100

    Inicio del proyecto

---
+++ conecta_db.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "localhost";
+    $username = "mitiendaonline";
+    $password = "";
+    $db = "mitiendaonline";
+    /**
+     * Mediante MySQLi
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+
+    // Check connection
+    if($conn -> connect_error){
+        die ("Conexion fallida: " . $conn -> connect_error);
+    }
+    echo "Conexion correcta";
+    */
+    
+    //Uso de PDO. Es mejor porque es versatil y facil migracion a otros gestores DDBB
+    // Create connection
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+        // set the PDO error mode to exception
+        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexion exitosa";
+    } catch (PDOException $e) {
+        echo "Conexio fallisa " . $e->getMessage();
+    }
+
+    echo "<br>He llegado al final"; //Comprobar la ejecucion el code
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styleIndex.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet"></head>
+<body>
+    <header class="nav_container">
+        <nav class="central_nav">
+            <ul>
+                <li class="list_nav">Crear producto</li>
+                <li class="list_nav">Consultar listado</li>
+                <li class="list_nav">Modificar producto</li>
+                <li class="list_nav">Eliminar producto</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
