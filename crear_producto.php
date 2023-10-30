ef691b41a55f74e2c547ff18bc92bdad2e9c81a0
Victor Plagaro Moreno <plagaro26@gmail.com>
2023-10-30 12:20:26 +0000

    Creacion de la hoja de estilos global

--- crear_producto.php
+++ crear_producto.php
@@ -2,18 +2,3 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion de la tienda online</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>
-            <input type="text">
-        </label>
-    </form>
-</body>
-</html>
--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styleIndex.css">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet"></head>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet">
+</head>
 <body>
     <header class="nav_container">
         <nav class="central_nav">
