0e4a46b109c7c652d78bb7d5181148efc7a81f4b
Victor-Plagaro <plagaro26@gmail.com>
2023-12-04 19:09:58 +0000

    no validos

--- form.php
+++ form.php
@@ -1,29 +1,50 @@
 <?php
-    // Revisa la conexión a la base de datos
-    include("./conecta_db.php");
-    try{
-        if(isset($_POST['nombreUsuario'])  && isset($_POST['pswd'])){
-            $nombreUsuario = $_POST['nombreUsuario'];
-            $correoUsuario = $_POST['email'];
+include("./conecta_db.php");
+try{
+    if(isset($_POST['nombreUsuario']) && isset($_POST['email']) && isset($_POST['pswd']))
+    {
+        $nombreUser=$_POST['nombreUsuario'];
+        $correoUser=$_POST['email'];
+        
+        session_start();
+        $_SESSION['usuario']=$nombreUser;
 
-            session_start();
-            $_SESSION['usuario']= $nombreUsuario;
+        $sql = $conn->prepare("SELECT contrasena_hash FROM usuarios WHERE nombre ='$nombreUser'");
+        $sql->execute();
+        $consulta=$sql->fetch(PDO::FETCH_ASSOC);
+        $pswdUser=$consulta['contrasena_hash'];
+    
+        if(password_verify($_POST['pswd'],$pswdUser))
+        {
+            echo("Contraseña correcta");
 
-            //Consultar
-            $sql = $conn -> prepare("SELECT contrasena_hash FROM usuarios2 WHERE nombre='$nombreUsuario'");
+            //Consulta para coger el color que quiere el usuario
+            $sql = $conn->prepare("SELECT color_fondo FROM usuarios WHERE nombre ='$nombreUser'");
             $sql->execute();
-            $pswdUsuario = $sql->fetch(PDO::FETCH_ASSOC);
-            $pswdEncriptada = $pswdUsuario['contrasena_hash'];
-            print_r($pswdUsuario);
-            if(password_verify($_POST['pswd'],$pswdEncriptada)){
-                echo "Contraseña Correcta";
-                header('Location: ./consultar_productos.php');
+            $consulta=$sql->fetch(PDO::FETCH_ASSOC);
+            $color = $consulta['color_fondo'];
+
+            //Creamos la cookie para asignar el color segun el usuario
+            if($color != null){
+                setcookie("color_fondo", $color, time()+3600*24);
             }else{
-                echo "Contraseña Incorrecta";
+                setcookie("color_fondo", "gray", time()+3600*24);
             }
-        }
-    }catch (PDOException $e){
-        echo "Error al recuperar los datos: " . $e->getMessage();
-        die();
-    } 
+            
+            //Redirigimos al usuario
+            header('Location: ./listar_productos.php');
+        }else{
+            if(!isset($_COOKIE['errores_login'])){
+                setcookie("errores_login", 1);
+            }else{
+                $contador_errores = $_COOKIE['errores_login'];
+                $contador_errores++;
+                setcookie
+            }
+        } 
+    }
+}catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
 ?>
